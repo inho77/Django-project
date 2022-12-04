0370657230e26641fafe94f87e38977b4445e5cc
inho77 <aliyusalisuboy@gmail.com>
2022-12-04 14:46:30 -0800

    updating contents

--- django_srms/settings.py
+++ django_srms/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,13 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR, 'static')
-    # BASE_DIR / "static",
+    BASE_DIR / "static",
     ]
     
-LOGIN_URL = 'login'
 
 #MEDIA
 MEDIA_URL = 'media/'
