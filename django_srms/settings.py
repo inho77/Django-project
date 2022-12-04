6e85e23999ffdc81a9a7121dfb880415dbc95a67
inho77 <aliyusalisuboy@gmail.com>
2022-12-04 15:03:19 -0800

    updating contents

--- django_srms/settings.py
+++ django_srms/settings.py
@@ -119,14 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [ 
     os.path.join(BASE_DIR, 'static')
     # BASE_DIR / "static",
     ]
     
-LOGIN_URL = 'login'
+#LOGIN_URL = 'login'
 
 #MEDIA
 MEDIA_URL = 'media/'
