7b136c69062afb5769afed451898cf8303537d7f
inho77 <aliyusalisuboy@gmail.com>
2022-12-04 22:54:11 -0800

    contents

--- django_srms/settings.py
+++ django_srms/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.whitenNoiseMiddleware',
+    # 'whitenoise.middleware.whitenNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
