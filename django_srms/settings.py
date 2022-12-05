b22a5f4f985d3c1f92f97acebe6b9e782b376364
inho77 <aliyusalisuboy@gmail.com>
2022-12-04 23:48:29 -0800

    contents

--- django_srms/settings.py
+++ django_srms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t5b_ld!b44gip%^24jv8w076iul#09*eiza#^$!n(%y6z=*=58
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','0.0.0.0']
 
 
 # Application definition
